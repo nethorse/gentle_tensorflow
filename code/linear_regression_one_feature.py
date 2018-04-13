65cdd9317cc86f864a439cd1ce333f9bac46ad7d
zhengxijun <zhengxijun@dvt.dvt.com>
2018-04-13 17:34:44 +0800

    add demo
    Signed-off-by: zhengxijun <zhengxijun@dvt.dvt.com>

--- code/linear_regression_one_feature.py
+++ code/linear_regression_one_feature.py
@@ -1,16 +1,17 @@
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.framework.graph_util import convert_variables_to_constants  
 
 # Model linear regression y = Wx + b
-x = tf.placeholder(tf.float32, [None, 1])
-W = tf.Variable(tf.zeros([1,1]))
-b = tf.Variable(tf.zeros([1]))
+x = tf.placeholder(tf.float32, [None, 1], name="x")
+W = tf.Variable(tf.zeros([1,1]), name="W")
+b = tf.Variable(tf.zeros([1]), name="b")
 product = tf.matmul(x,W)
-y = product + b
-y_ = tf.placeholder(tf.float32, [None, 1])
+y = tf.add(product, b, name="y")
+y_ = tf.placeholder(tf.float32, [None, 1], name="y_")
 
 # Cost function sum((y_-y)**2)
-cost = tf.reduce_mean(tf.square(y_-y))
+cost = tf.reduce_mean(tf.square(y_-y), name="cost")
 
 # Training using Gradient Descent to minimize cost
 train_step = tf.train.GradientDescentOptimizer(0.0000001).minimize(cost)
@@ -33,3 +34,10 @@ for i in range(steps):
   print("cost: %f" % sess.run(cost, feed_dict=feed))
 
 # NOTE: W should be close to 2, and b should be close to 0
+feed = { x: [[4],[16]] }
+print(sess.run(y, feed_dict=feed))
+
+graph = convert_variables_to_constants(sess, sess.graph_def, ["y"]) #out为保存网络的最后输出节点名称  
+tf.train.write_graph(graph, '.', 'graph.pb', as_text=False)  
+writer = tf.summary.FileWriter("logs/",sess.graph)
+print(tf.global_variables())
